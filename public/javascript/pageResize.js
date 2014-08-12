673c49b165c470fb30373a55425fc97c8339131e
pskhadke <pskhadke@.us.ibm.com>
2014-08-12 15:36:16 -0400

    Work Item 120882: Code Review Changes

--- public/javascript/pageResize.js
+++ public/javascript/pageResize.js
@@ -3,10 +3,15 @@
  */
 
 $(document).ready(function(){
+	resizePg();
+	$(window).on('resize', resizePg());
+});
+
+var resizePg = function() {
 	var docHeight = $(window).height();
 	var navbarHeight = $("#navbar").height();
 	var footerHeight = $("#footer").height();
 	var contentHeight = $("#content").height();
 	var minContentHeight = Math.max(docHeight - navbarHeight - footerHeight, contentHeight);
-	$("#content").css('min-height', minContentHeight);
-});
+	$("#content").css('min-height', minContentHeight);	
+};
