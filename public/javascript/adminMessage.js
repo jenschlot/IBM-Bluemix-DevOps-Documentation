35112135b6f36b62d1ce0448b65b11ab35a49e59
pskhadke <pskhadke@.us.ibm.com>
2014-08-15 14:27:57 -0400

    Work Item 115412: Dynamically update the maintenance bar message

--- public/javascript/adminMessage.js
+++ public/javascript/adminMessage.js
@@ -4,7 +4,10 @@
 
 $(document).ready(function() {
 	$.get("/manage/service/com.ibm.team.jazzhub.common.service.IServerService/message", function(response) {
-		console.log(response);
 		$("#messageSummary").html(response.message);
+		if(response.message.length > 0) {
+			$("#announcement").removeClass("hidden");
+		}
 	});
 });
+
