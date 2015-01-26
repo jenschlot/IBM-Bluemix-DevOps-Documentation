b095eb5ab69a43af1159cc7798d3a665db01de10
Ross Anderson <aross@ca.ibm.com>
2015-01-26 11:43:31 -0500

    sidebar: highlight active page, even if child element

--- public/javascript/sidebarSelectElement.js
+++ public/javascript/sidebarSelectElement.js
@@ -1,19 +1,51 @@
 
 $(document).ready(function() {
-	applySidebarSelectedElementStyles();
+	refreshActiveElement();
 });
 
-function applySidebarSelectedElementStyles() {
+function hideAllChildElements() {
+	$(".jh-sidebar-full-content .sidebar-container .nav .item .item").hide({
+		duration: 300
+	});
+}
+
+function showActiveElement(element) {
+	var $element = $(element);
+	$element.show();
+	$element.parent().show();
+	$element.siblings().show({
+		duration: 300
+	});
+	$element.children().show({
+		duration: 300
+	});
+}
+
+function findActiveElement() {
+	var activeElement = null;
 	$(".jh-sidebar-full-content .sidebar-container .nav .item").each(function() {
 		var a = $(this).children("a");
-		if (normalize(window.location.pathname) === normalize(a.attr('href'))) {
+		var curPath = window.location.pathname;
+		curPath += window.location.hash;
+		if (normalizeEqual(curPath, a.attr('href'))) {
 			$(this).addClass('selected');
+			activeElement = this;
 		} else {
 			$(this).removeClass('selected');
 		}
 	});
+	return activeElement;
 }
 
-function normalize(path) {
-	return path.toLowerCase().replace(/\//g, '');
+function refreshActiveElement() {
+	hideAllChildElements();
+	var activeElement = findActiveElement();
+	showActiveElement(activeElement);
+}
+
+function normalizeEqual(path_a, path_b) {
+	function normalize(path) {
+		return path.toLowerCase().replace(/\//g, '');
+	}
+	return normalize(path_a) === normalize(path_b);
 }
